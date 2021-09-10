44edc0893ff27fbb5d86bd12ac0bbc61a5838981
Muhammad Sohaib Ali <sohaibayub9@gmail.com>
2021-09-10 12:49:38 +0500

    Travis Updated

--- tests/test_myapp.py
+++ tests/test_myapp.py
@@ -2,100 +2,208 @@ import os
 import tempfile
 
 import pytest
+import unittest
 
 from myapp import create_app
 from myapp import db
 
 
-@pytest.fixture
-def client():
-    global app
-    db_fd, db_path = tempfile.mkstemp()
-    app = create_app()
+# @pytest.fixture
+class BasicTests(unittest.TestCase):
+    def __init__(self, methodName: str) -> None:
+        self.app = create_app()
 
-    with app.test_client() as client:
-        with app.app_context():
-            db.init_app(app)
-        yield client
+    def client(self):
+        # global app
+        db_fd, db_path = tempfile.mkstemp()
 
-    os.close(db_fd)
-    os.unlink(db_path)
+        with self.app.test_client() as client:
+            with self.app.app_context():
+                db.init_app(self.app)
+            yield client
 
-def test_home_page(client):
-    """Start with a blank database."""
+        os.close(db_fd)
+        os.unlink(db_path)
 
-    rv = client.get('/')
-    assert b'Welcome To this Site!' in rv.data
+    def test_home_page(self, client):
+        """Start with a blank database."""
 
-def login(client, email, password):
-    return client.post('/login', data=dict(
-        email=email,
-        password=password
-    ), follow_redirects=True)
+        rv = client.get('/')
+        assert b'Welcome To this Site!' in rv.data
+
+    def login(self, client, email, password):
+        return client.post('/login', data=dict(
+            email=email,
+            password=password
+        ), follow_redirects=True)
 
 
-def logout(client):
-    return client.get('/logout', follow_redirects=True)
+    def logout(self, client):
+        return client.get('/logout', follow_redirects=True)
 
-def profile_page(client):
-    return client.get('/profile', follow_redirects=True)
+    def profile_page(self, client):
+        return client.get('/profile', follow_redirects=True)
 
-def posts_page(client):
-    return client.get('/api/getposts', follow_redirects=True)
+    def posts_page(self, client):
+        return client.get('/api/getposts', follow_redirects=True)
 
-def test_login_logout(client):
-    """Make sure login and logout works."""
+    def test_login_logout(self, client):
+        """Make sure login and logout works."""
 
-    email = app.config["EMAIL"]
-    password = app.config["PASSWORD"]
+        email = self.app.config["EMAIL"]
+        password = self.app.config["PASSWORD"]
 
-    rv = login(client, email, password)
-    assert b'Welcome, to Your Profile' in rv.data
+        rv = self.login(client, email, password)
+        assert b'Welcome, to Your Profile' in rv.data
 
-    rv = logout(client)
-    assert b'You are Not Logged In, Please Login to Enjoy Full Features' in rv.data
+        rv = self.logout(client)
+        assert b'You are Not Logged In, Please Login to Enjoy Full Features' in rv.data
 
-    rv = login(client, f"{email}x", password)
-    assert b'Check your Login Credentials and Try Again' in rv.data
+        rv = self.login(client, f"{email}x", password)
+        assert b'Check your Login Credentials and Try Again' in rv.data
 
-    rv = login(client, email, f'{password}x')
-    assert b'Check your Login Credentials and Try Again' in rv.data
+        rv = self.login(client, email, f'{password}x')
+        assert b'Check your Login Credentials and Try Again' in rv.data
 
-def test_profile_for_loggedin(client):
-    """Make sure Profile is Visible if user is logged In"""
+    def test_profile_for_loggedin(self, client):
+        """Make sure Profile is Visible if user is logged In"""
 
-    email = app.config["EMAIL"]
-    password = app.config["PASSWORD"]
+        email = self.app.config["EMAIL"]
+        password = self.app.config["PASSWORD"]
 
-    _ = login(client, email, password)
+        _ = self.login(client, email, password)
 
-    rv = profile_page(client)
-    assert b'Welcome, to Your Profile' in rv.data
+        rv = self.profile_page(client)
+        assert b'Welcome, to Your Profile' in rv.data
 
-def test_profile_for_loggedout(client):
-    """Make sure Profile is not Visible if user is logged Out"""
+    def test_profile_for_loggedout(self, client):
+        """Make sure Profile is not Visible if user is logged Out"""
 
-    rv = profile_page(client)
-    if b'Please log in to access this page' in rv.data:
-        print('[-]  User not logged in')
-        assert True
+        rv = self.profile_page(client)
+        if b'Please log in to access this page' in rv.data:
+            print('[-]  User not logged in')
+            assert True
 
-def test_posts_for_loggedin(client):
-    """Make sure POSTS is Visible if user is logged In"""
+    def test_posts_for_loggedin(self, client):
+        """Make sure POSTS is Visible if user is logged In"""
 
-    email = app.config["EMAIL"]
-    password = app.config["PASSWORD"]
+        email = self.app.config["EMAIL"]
+        password = self.app.config["PASSWORD"]
 
-    _ = login(client, email, password)
+        _ = self.login(client, email, password)
 
-    rv = posts_page(client)
-    assert b'Welcome To Posts Page' in rv.data
+        rv = self.posts_page(client)
+        assert b'Welcome To Posts Page' in rv.data
 
-def test_posts_for_loggedout(client):
-    """Make sure POSTS is not Visible if user is logged Out"""
+    def test_posts_for_loggedout(self, client):
+        """Make sure POSTS is not Visible if user is logged Out"""
 
-    rv = posts_page(client)
-    if b'Please log in to access this page' in rv.data:
-        print('[-]  User not logged in')
-        assert True
+        rv = self.posts_page(client)
+        if b'Please log in to access this page' in rv.data:
+            print('[-]  User not logged in')
+            assert True
+
+if __name__ == "__main__":
+    unittest.main()
+
+# import os
+# import tempfile
+
+# import pytest
+
+# from myapp import create_app
+# from myapp import db
+
+
+# @pytest.fixture
+# def client():
+#     global app
+#     db_fd, db_path = tempfile.mkstemp()
+#     app = create_app()
+
+#     with app.test_client() as client:
+#         with app.app_context():
+#             db.init_app(app)
+#         yield client
+
+#     os.close(db_fd)
+#     os.unlink(db_path)
+
+# def test_home_page(client):
+#     """Start with a blank database."""
+
+#     rv = client.get('/')
+#     assert b'Welcome To this Site!' in rv.data
+
+# def login(client, email, password):
+#     return client.post('/login', data=dict(
+#         email=email,
+#         password=password
+#     ), follow_redirects=True)
+
+
+# def logout(client):
+#     return client.get('/logout', follow_redirects=True)
+
+# def profile_page(client):
+#     return client.get('/profile', follow_redirects=True)
+
+# def posts_page(client):
+#     return client.get('/api/getposts', follow_redirects=True)
+
+# def test_login_logout(client):
+#     """Make sure login and logout works."""
+
+#     email = app.config["EMAIL"]
+#     password = app.config["PASSWORD"]
+
+#     rv = login(client, email, password)
+#     assert b'Welcome, to Your Profile' in rv.data
+
+#     rv = logout(client)
+#     assert b'You are Not Logged In, Please Login to Enjoy Full Features' in rv.data
+
+#     rv = login(client, f"{email}x", password)
+#     assert b'Check your Login Credentials and Try Again' in rv.data
+
+#     rv = login(client, email, f'{password}x')
+#     assert b'Check your Login Credentials and Try Again' in rv.data
+
+# def test_profile_for_loggedin(client):
+#     """Make sure Profile is Visible if user is logged In"""
+
+#     email = app.config["EMAIL"]
+#     password = app.config["PASSWORD"]
+
+#     _ = login(client, email, password)
+
+#     rv = profile_page(client)
+#     assert b'Welcome, to Your Profile' in rv.data
+
+# def test_profile_for_loggedout(client):
+#     """Make sure Profile is not Visible if user is logged Out"""
+
+#     rv = profile_page(client)
+#     if b'Please log in to access this page' in rv.data:
+#         print('[-]  User not logged in')
+#         assert True
+
+# def test_posts_for_loggedin(client):
+#     """Make sure POSTS is Visible if user is logged In"""
+
+#     email = app.config["EMAIL"]
+#     password = app.config["PASSWORD"]
+
+#     _ = login(client, email, password)
+
+#     rv = posts_page(client)
+#     assert b'Welcome To Posts Page' in rv.data
+
+# def test_posts_for_loggedout(client):
+#     """Make sure POSTS is not Visible if user is logged Out"""
+
+#     rv = posts_page(client)
+#     if b'Please log in to access this page' in rv.data:
+#         print('[-]  User not logged in')
+#         assert True
 
